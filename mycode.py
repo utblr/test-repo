8239e33e3b3c71f93f8a9ca21b127634d4031bf9
utblr <ufuktan@utblr.xyz>
2025-04-16 13:17:47 +0300

    add hello() function

--- mycode.py
+++ mycode.py
@@ -1,1 +1,11 @@
 print("this is a new file")
+
+# define a function that says "hello <your-name>"
+
+def hello(name):
+    print("hello %s" % name)
+
+
+name = input("enter your name: ")
+
+hello(name)
